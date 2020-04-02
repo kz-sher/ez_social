656e143198afdf7363dbb3094c422c3d9ceaa19c
Macbie <kongzhengho@Kongs-MacBook-Air.local>
2020-04-02 21:29:33 +0800

    Redesign 404 page

--- client/src/containers/NotFound.js
+++ client/src/containers/NotFound.js
@@ -1,32 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, Button, Typography, Divider, Box } from '@material-ui/core';
+import { Grid, Button, Typography, Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Emoji from '../images/emoji.png';
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+        fontWeight: 400,
+        fontSize: '70px',
+        textAlign: 'center',
+        [theme.breakpoints.up('sm')]: {
+            textAlign: 'left',
+        }
+    },
+    bull: {
+        fontSize: '50px'
+    },
+    body: {
+        margin: theme.spacing(0.5, 0, 2),
+        textAlign: 'center',
+        [theme.breakpoints.up('sm')]: {
+            textAlign: 'left',
+        }
+    },
+  }));
 
 const NotFound = () => {
-    const bull = <span> • </span>;
+    const classes = useStyles();
+    const bull = <span className={classes.bull}> • </span>;
     return ( 
-        <Grid className="main-container" container item direction="row" justify="center" alignItems="center" xs={11} sm={6} spacing={3}>
+        <Grid container item direction="row-reverse" justify="space-between" alignItems="center" xs={11} sm={6} spacing={3}>
             
-            <Grid item xs={12} sm={12}>
-                <Typography variant="h3"> 
-                    <Box fontWeight={400}>
-                        &lt; 4{bull}0{bull}4 &gt; 
-                    </Box>
-                </Typography> 
-                <Typography variant="caption"> Page Not Found </Typography> 
-                <Divider variant="middle" />
+            <Grid container item justify="center" sm={6}>
+                <img src={Emoji} width='90%' height='90%'/>
             </Grid>
 
-            <Grid container item justify="center" alignItems="center" spacing={3}>
-                <Grid item>
-                    <Typography variant="body1">
+            <Grid container item justify="center" sm={6}>
+                <Grid item xs={8} sm={12}>
+                    <Typography className={classes.title} variant="h3"> 
+                            4{bull}0{bull}4
+                    </Typography> 
+                    <Typography variant="caption"> Page Not Found </Typography> 
+                    <Divider variant="fullWidth" />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Typography className={classes.body} variant="body1">
                         Sorry, this content isn't available right now.
                     </Typography>
                 </Grid>
-            </Grid>
 
-            <Grid item xs={12}>
-                <Button component={Link} to="/" size="large" variant="contained">Back</Button>
+                <Grid item sm={12}>
+                    <Button component={Link} to="/" size="large" variant="contained">Back</Button>
+                </Grid>
             </Grid>
 
         </Grid>
